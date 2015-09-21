6f16455fca4f2d8d91c72b89d0fc11c91b77d97a
Veselin Vasilev <vesojob@users.noreply.github.com>
2015-09-21 09:23:58 +0300

    Create value in ContextBroker

--- arduino-contextBroker.ino
+++ arduino-contextBroker.ino
@@ -1,25 +1,17 @@
-/*
-Arduino RFID ContextBroker 
-
-*/
 #include <SPI.h>
 #include <SoftwareSerial.h>
-#include <HttpClient.h>
-#include <Wire.h>
 
 //memory check
 #include <MemoryFree.h>
 #include <pgmStrToRAM.h>
-
-//TIME
-#include <DS1307RTC.h>
-#include <Time.h>
 
 //Ethernet shield
 #include <Ethernet.h>
 
 //Light meter
 #include "TSL2561.h"
+#include <Wire.h>
+#include <RTClib.h>
 
 //RFID
 #include <MFRC522.h>
@@ -33,11 +25,15 @@ Arduino RFID ContextBroker
 #define msgErr "Fail"
 #define msgReady "Ready"
 
+//SENSOR ID
+#define sid       10606353;
+
 //ContextBroker
 #define url             "95.111.115.171"  
 #define PORT            1026 
-#define MSG_BUF_LEN     20
-#define JSON_BUF_LEN    200
+
+//TIME
+RTC_DS1307 RTC;
 
 //TMP36 Pin Variables
 int sensorPin = 0;
@@ -55,8 +51,9 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 //Ethernet config
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
+//Client
 EthernetClient client;
-HttpClient http(client);
+
 
 void setup(){
     //Init serial bus
@@ -64,6 +61,11 @@ void setup(){
     
     //Init SPI bus
     SPI.begin();      
+    
+    //Init Time
+    Wire.begin();
+    RTC.begin();
+ 
     
     //Init LCD
     lcd.begin(16, 2);
@@ -84,10 +86,7 @@ void setup(){
     delay(1000);  //give the Ethernet shield a second to initialize:
     
     lcd.clear();
-  
-    //Serial.print("IP=");
-    //lcd.print("IP=");
-    
+      
     Serial.println(Ethernet.localIP());
     lcd.print(Ethernet.localIP());
     
@@ -101,49 +100,34 @@ void setup(){
     lcd.setCursor(0, 1);
     Serial.println(msgReady); 
     lcd.print(msgReady);
-    delay(3000); 
+    
+    delay(2000); 
 }
 
 void loop() {
 
-  //TIME
-  tmElements_t tm;
-  RTC.read(tm);
- // /*
-  char szResVal[4];
-  String timestamp;
-  Serial.print("Timestamp = ");
   lcd.clear();
-  Serial.print(tmYearToCalendar(tm.Year));
-  sprintf(szResVal,"%02u", tmYearToCalendar(tm.Year)) ;
-  timestamp = szResVal;
-  //Serial.println(timestamp);
-  sprintf(szResVal,"%02u", tm.Month) ;
-  Serial.print(szResVal);
-  timestamp=timestamp+szResVal;
-  //Serial.println(timestamp);
-  sprintf(szResVal,"%02u", tm.Day) ;
-  Serial.print(szResVal);
-  lcd.setCursor(0, 1);
-  timestamp=timestamp+szResVal;
-  //Serial.println(timestamp);
-  sprintf(szResVal,"%02u", tm.Hour) ;
-  lcd.print(szResVal);
-  Serial.print(szResVal);
-  timestamp=timestamp+szResVal;
-  lcd.print(":");
-  sprintf(szResVal,"%02u", tm.Minute) ;
-  lcd.print(szResVal);
-  Serial.print(szResVal);
-  timestamp=timestamp+szResVal;
-  lcd.print(":");
-  sprintf(szResVal,"%02u", tm.Second) ;
-  lcd.print(szResVal);
-  Serial.print(szResVal);
-  timestamp=timestamp+szResVal;
-  Serial.println();
-  //Serial.println(timestamp);
- // */
+  DateTime now = RTC.now();
+    Serial.print("Timestamp= ");
+    Serial.println(now.unixtime());
+    Serial.print(now.year());
+    Serial.print('/');
+    Serial.print(now.month());
+    Serial.print('/');
+    Serial.print(now.day());
+    Serial.print(' ');
+    Serial.print(now.hour());
+    Serial.print(':');
+    Serial.print(now.minute());
+    Serial.print(':');
+    Serial.println(now.second());
+    
+    lcd.setCursor(0, 1);
+    lcd.print(now.hour());
+    lcd.print(":");
+    lcd.print(now.minute());
+    lcd.print(":");
+     lcd.print(now.second());
   
   //temperature
   int reading = analogRead(sensorPin);  
@@ -152,28 +136,34 @@ void loop() {
   Serial.print(voltage); Serial.println(" volts");
   float temperatureC = (voltage - 0.5) * 100 ;  
   Serial.print(temperatureC); Serial.println(" degrees C");
-  
+   
+   char str_temp[6];
+   /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
+     dtostrf(temperatureC, 4, 2, str_temp);
+     //sprintf(temperature,"%s F", str_temp);
 
- // This can take 13-402 milliseconds! Uncomment whichever of the following you want to read
-  uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);     
-  uint32_t lum = tsl.getFullLuminosity();
-  uint16_t ir, full;
-  ir = lum >> 16;
-  full = lum & 0xFFFF;
-  //Serial.print("Visible: "); Serial.print(full - ir);   Serial.print("\t");
-  //Serial.print("Lux: "); Serial.println(tsl.calculateLux(full, ir));
-  lcd.setCursor(0, 0);
-  lcd.print("T=");
- // lcd.setCursor(2, 0);
-  lcd.print(temperatureC);
-  lcd.setCursor(9, 0);
-  lcd.print("L=");
-  //lcd.setCursor(11, 0);
-  lcd.print(full - ir);
+    Serial.println(str_temp);
+
+    // This can take 13-402 milliseconds! Uncomment whichever of the following you want to read
+    uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);     
+    uint32_t lum = tsl.getFullLuminosity();
+    uint16_t ir, full;
+    ir = lum >> 16;
+    full = lum & 0xFFFF;
+    //Serial.print("Visible: "); Serial.print(full - ir);   Serial.print("\t");
+    //Serial.print("Lux: "); Serial.println(tsl.calculateLux(full, ir));
+    lcd.setCursor(0, 0);
+    lcd.print("T=");
+    // lcd.setCursor(2, 0);
+    lcd.print(temperatureC);
+    lcd.setCursor(9, 0);
+    lcd.print("L=");
+    //lcd.setCursor(11, 0);
+    lcd.print(full - ir);
 
   delay(1000);
         
-        //RFID     
+        //RFID card operation     
         int progress;
         // Look for new cards
         if ( ! mfrc522.PICC_IsNewCardPresent()) {
@@ -183,17 +173,22 @@ void loop() {
         if ( ! mfrc522.PICC_ReadCardSerial()) 
         { return;  }
         
-        Serial.print("Card UID:");
+        
         
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.print("Card UID:");
+        lcd.print("Sending..");
         
         Serial.print("  freeMemory()=");
         Serial.println(freeMemory());
-
-        progress = sendCardId(dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size), timestamp,temperatureC );
         
+        //Genereting Card serial niumber
+               
+        progress = sendContext(dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size) , now.unixtime());   
+        //Serial.println(progress);
+        
+        
+       
         //lcd.clear();
         //lcd.setCursor(0, 0);
         //lcd.print(dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size));
@@ -207,156 +202,105 @@ void loop() {
             lcd.setCursor(10, 0);
             lcd.print(msgReady);
             
-        }else{
-            Serial.println(msgErr);
-            lcd.setCursor(10, 0);
-            lcd.print(msgErr);
-                      
-        } 
-        Serial.println();        
+        }
+           
   
   delay(3000);
 }
 
-String dump_byte_array(byte *buffer, byte bufferSize) {
-    String uid;
-    
-    for (byte i = 0; i < bufferSize; i++) {
-       
-        //Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        //Serial.print(buffer[i], HEX); 
-
-        //if (i >0)
-        //{
-        //  uid += buffer[i] < 0x10 ? "0" : " ";
-        //}
-        uid += buffer[i];
-    }
-
-    //Serial.println();    
-    //Serial.print("uid:");    
-    Serial.print(uid);
-    Serial.println();
-    lcd.setCursor(0, 1);
-    lcd.print(uid);
-    return uid;
+long dump_byte_array(byte *buffer, byte bufferSize) {
+   
+    String sID;
+        if (mfrc522.uid.size <= 4)
+        {
+           unsigned long nNumber = 0 ;
+            for (int nCurrByte = 0; nCurrByte < mfrc522.uid.size; nCurrByte++ )
+              {
+                  unsigned long nTempVal = mfrc522.uid.uidByte[nCurrByte]; //left bytes side read
+                  //unsigned long nTempVal = mfrc522.uid.uidByte[((mfrc522.uid.size-1) -   nCurrByte)]; //right bytes side read
+              
+                  nTempVal<<= (((mfrc522.uid.size-1) -   nCurrByte) * 8);
+                  nNumber += nTempVal;
+              }
+              Serial.println(nNumber);
+              return  nNumber;
+        }
+        else
+        {
+          Serial.println("Error! source buffer is larger than 4 bytes!");
+        }
 }
 
-int sendCardId(String tag, String tstamp, float temp) {
 
-        Serial.print(msgSend);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print(msgSend);
+
+int sendContext( unsigned long tag, unsigned long timestamp ){
         lcd.setCursor(0, 1);
         lcd.print(tag);
-    
-    Serial.print("  freeMemory()=");
-    Serial.println(freeMemory());
-    //Serial.println(tag);
-char str_temp[6];
+if (client.connect(url, PORT)) {
+      long conLength;
+       String data;
+       String data1;
+       String data2;
+        String data9;
+      String kg="1.3";
       
-      /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
-      dtostrf(temp, 4, 2, str_temp);
-     // sprintf(temperature,"%s F", str_temp);
-  
-        char msgBuffer[MSG_BUF_LEN], valueBuffer[JSON_BUF_LEN];
-        int  msgContentLength = 0;
-        char tagValue[32];
-        char tstampValue[32];
-        tag.toCharArray(tagValue, 32);
-        tstamp.toCharArray(tstampValue, 32);
-        sprintf(valueBuffer
-                , "%s%s%s%s%s%s%s%s%s%s%s%s"
-                ,"{\"contextElements\":[{\"type\":\"f\",\"isPattern\":\"false\",\"id\":\""
-                , tagValue
-                ,"\",\"attributes\":["
-                ,"{\"name\":\""
-                ,"ts\",\"type\":\"string\",\"value\":\""
-                , tstampValue
-                , "\"},"
-                ,"{\"name\":\""
-                ,"t2\",\"type\":\"float\",\"value\":\""
-                , str_temp
-                , "\"}"
-                ,"]}],\"updateAction\":\"APPEND\"}\r\n\r\n\r\n");
+      data = "{\"contextElements\":[{\"type\":\"b\",\"isPattern\":\"false\",\"id\":\"";
+      //Serial.println(data);
+      data = data + tag;
+      //Serial.println(data);
+      data= data + "\",\"attributes\":[{\"name\":\"ts\",\"type\":\"string\",\"value\":\"";
+      data = data + timestamp;
+     // Serial.print(data);
+     
+      data1 = "\"},{\"name\":\"sid\",\"type\":\"string\",\"value\":\"";
+       data1 = data1 + sid;
         
-        int result;           
-        memset(msgBuffer , 0 , MSG_BUF_LEN);
-        http.beginRequest();
-        result = http.startRequest(url, PORT, "/ngsi10/updateContext", HTTP_METHOD_POST, "Arduino");
-        
-         Serial.println(valueBuffer);  
-              
-        if(result == HTTP_ERROR_API){
-          Serial.println(F("API error"));
-          http.endRequest();       
-          http.stop();
-          return 1;     
-        }else if(result == HTTP_ERROR_CONNECTION_FAILED){
-            Serial.println(F("Connection failed"));   
-            http.endRequest();   
-            http.stop();
-            return 1;                 
-        }else if( result == HTTP_SUCCESS ){     
-            
-            //Serial.println("begin HTTP request");         
-            
-            memset( msgBuffer , 0 , MSG_BUF_LEN );
-            //http.sendHeader(msgBuffer);
-            http.sendHeader("Accept: application/json");
-            http.sendHeader("Content-Type: application/json");
-            msgContentLength = strlen(valueBuffer) - 1 ;  
-            
-            //Serial.print(F("  freeMemory()="));
-            //Serial.println(freeMemory());
-            //Serial.print(valueBuffer);
-            
-            memset(msgBuffer , 0 , MSG_BUF_LEN);
-            sprintf( msgBuffer , "Content-Length: %d" , msgContentLength );
-            http.sendHeader(msgBuffer);
-            http.write((const uint8_t*) valueBuffer , msgContentLength + 2 );          
-            http.endRequest();
-            
-            //Serial.println(F("HTTP request end"));
-            
-            int err =0;
-            err = http.responseStatusCode();
-            if(err < 0){
-              Serial.println(err);
-              http.stop();
-              return 1;
-            }
-            
-            err = http.skipResponseHeaders();
-            
-            //Serial.println(err);                       
-            
-            char c;
-            int count = 0;
-            boolean saveNow =false;
-            while(1){
-              c = http.read();
-            //Print responce from server
-              Serial.print(c);              
-            
-              if(c == '"'){
-                saveNow = true;
-                count++;    
-             }else if(saveNow && count < 2){
-                       
-              }else if(count >= 2){
-                break;
-              }  
-            }                                                                 
-            Serial.println();
-            
-            http.stop();     
-            return 0;            
-        }else{
-            http.endRequest();   
-            http.stop();                                   
-           return 1;
-        }           
-        
-  }
+        data2 = "\"},{\"name\":\"kg\",\"type\":\"float\",\"value\":\"";
+       data2 = data2 + kg;
+
+      //Serial.println(data1);
+      data9 = "\"}]}],\"updateAction\":\"APPEND\"}";
+      
+      //Check Context Length: 
+      conLength = data.length();
+      conLength = conLength +  data1.length();
+      conLength =  conLength+  data2.length();
+      conLength =  conLength+  data9.length();
+     // Serial.println(data2);
+      Serial.println("connected");
+      
+      
+      Serial.println(conLength);
+      // Make a HTTP request:
+      client.println("POST  /ngsi10/updateContext");
+      //client.println("Connection: keep-alive");
+      client.println("Accept: application/json");
+      client.println("Content-Type: application/json");
+      //client.println("Content-Type:   text/plain; charset=UTF-8");
+      //client.print("X-Auth-Token: "); 
+      //client.println("VMfS1db5lyV3mtJ7YSqAqPFHR9cu9m");
+    
+      //client.println("Connection: close");
+      client.print("Content-Length: ");
+      client.println(conLength);
+      client.println(); 
+      client.print(data);
+      client.print(data1);
+      client.print(data2);
+      client.print(data9);
+      client.println();
+      //client.println("Connection: close");
+      //Serial.println(data);
+      client.stop();
+      //delay (1000);
+      return 0;
+   }
+    else {
+    // if you didn't get a connection to the server:
+       Serial.println(msgErr);
+            lcd.setCursor(10, 0);
+            lcd.print(msgErr);
+   }  
+}
+
+ 
